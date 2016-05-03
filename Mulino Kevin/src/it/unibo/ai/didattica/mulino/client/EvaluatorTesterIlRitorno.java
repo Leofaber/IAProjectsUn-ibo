9c81b4852055e5dae782dfd312fadc0e5782d7af
Francesco <Francesco@DESKTOP-C1SRDLP.lan>
2016-05-03 16:37:21 +0200

    .

---
+++ Mulino Kevin/src/it/unibo/ai/didattica/mulino/client/EvaluatorTesterIlRitorno.java
@@ -0,0 +1,93 @@
+package it.unibo.ai.didattica.mulino.client;
+
+import it.unibo.ai.didattica.mulino.actions.Phase1;
+import it.unibo.ai.didattica.mulino.actions.Phase1Action;
+import it.unibo.ai.didattica.mulino.domain.State;
+import it.unibo.ai.didattica.mulino.domain.State.Checker;
+
+public class EvaluatorTesterIlRitorno {
+
+	public EvaluatorTesterIlRitorno() {
+		
+	}
+
+	public static void main(String[] args) {
+		State state=new State();
+		Phase1Action action;
+		try{						
+			action = new Phase1Action();
+			action.setPutPosition("d1");
+			state=Phase1.applyMove(state, action, Checker.WHITE);
+			
+			
+			action = new Phase1Action();
+			action.setPutPosition("d2");
+			state=Phase1.applyMove(state, action, Checker.WHITE);
+			
+			action = new Phase1Action();
+			action.setPutPosition("a4");
+			state=Phase1.applyMove(state, action, Checker.WHITE);
+			
+			
+			action = new Phase1Action();
+			action.setPutPosition("b4");
+			state=Phase1.applyMove(state, action, Checker.WHITE);
+			
+			action = new Phase1Action();
+			action.setPutPosition("f4");
+			state=Phase1.applyMove(state, action, Checker.BLACK);
+			
+			
+			action = new Phase1Action();
+			action.setPutPosition("g4");
+			state=Phase1.applyMove(state, action, Checker.BLACK);
+			
+			action = new Phase1Action();
+			action.setPutPosition("a7");
+			state=Phase1.applyMove(state, action, Checker.BLACK);
+			
+			
+			action = new Phase1Action();
+			action.setPutPosition("d7");
+			state=Phase1.applyMove(state, action, Checker.BLACK);
+
+			action = new Phase1Action();
+			action.setPutPosition("g1");
+			state=Phase1.applyMove(state, action, Checker.WHITE);
+			
+			action = new Phase1Action();
+			action.setPutPosition("f2");
+			state=Phase1.applyMove(state, action, Checker.BLACK);
+			
+			
+			
+			System.out.println(state.toString());
+			HeuristicEvaluator eval=new HeuristicEvaluator(state, Checker.BLACK);
+			
+			
+			/* * * * * * * * * * * * * * * * *
+			 * 	 CALA'S POWER FUNCTION CALL  * 
+			 * * * * * * * * * * * * * * * * */
+			state.setArrayOfMorris();
+			/* * * * * * * * * * * * * * * * * *
+			 *  END CALA'S POWER FUNCTION CALL *
+			 * * * * * * * * * * * * * * * * * */
+			
+			
+			System.out.println("TRIS BLACK-TRIS WHITE: "+ eval.numberOfMorris());
+			System.out.println("DOUBLE BLACK-DOUBLE WHITE: "+ eval.doubleMorris());
+			System.out.println("PEZZI BLACK-PEZZI WHITE: "+eval.numberOfPieces());
+			System.out.println("PEZZI WHITE BLOCCATI - BLACK BLOCCATI: "+eval.numberOfBlockedOpponentPieces());
+			System.out.println("DOUBLE MORRIS BLACK - DOUBLE MORRIS WHITE: "+eval.doubleMorris());
+			System.out.println("2PIECEBLACK - 2PIECEWHITE "+eval.numberOf2PieceConfigurations());
+ 
+		}catch(Exception e){
+			System.out.println("Exception: "+e);
+		}
+		
+		
+		
+		
+	}
+
+}
